876a1fd5d571f6a8379290c367dc13ed4d6ead60
Cesar Morales <cesarlenin2@live.com>
2020-04-06 09:22:23 -0700

    fix html syntax

--- src/composition/Info.js
+++ src/composition/Info.js
@@ -4,58 +4,49 @@ import './Info.css';
 function Info(props) {
     return (
         <div>
-            <header>
-                <h1>Sally Student Resume</h1>
-                <p></p>
-            </header>
-            <main><nav>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about_me">About me</a></li>
-                    <li><a href="/sallystudent@gmail.com">Contact</a></li>
-                </ul>
-            </nav>
-            </main>
-            <section>
-                <H1>About Me</H1>
-                <img src="https://m.media-amazon.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_.jpg" alt="A picture of myself" />
+            <main>
+                <section>
+                    <h2>About Sally Student</h2>
+                    <p lang="fr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae veritatis sed laborum, accusantium ut dolore asperiores ea enim, hic fuga corporis? Id cupiditate, voluptatibus veniam doloribus consectetur quia! Quae, eveniet?</p>
+                    <p lang="fr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste eligendi distinctio libero error vitae aliquid aut autem doloribus delectus necessitatibus possimus tempora, ipsam sequi, quae architecto maxime asperiores qui voluptate.</p>
+                    <p lang="fr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa est iusto doloribus consectetur fugit quis accusantium hic assumenda, dolorem autem dolor voluptatibus dolore sunt sed labore neque, saepe illum et!</p>
+                </section>
+  
+                <section>
+                    <h3>Sally's education:</h3>
+                    <ul>
+                        <li>grossmount collegue</li>
+                        <li>Butte Collegue</li>
+                        <li>Chico State University</li>
+                        <li>Thinkful</li>
+                    </ul>
+                </section>
 
-                <p>
-                    I am currently studying HTML code online. Coding is a passion of mine and I am very excited to be continuing my education in this field.
-</p>
+                <section>
+                    <h3>Sally's employment history:</h3> 
+                    <ul><li>Scribbs</li></ul>
+      
+                    <p>Job Title: Software Developer<br/> 
+                     Date of Employement: 2/17/2013-8/15/2017<br/> 
+                    Job Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae veritatis sed laborum, accusantium ut dolore asperiores ea enim, hic fuga corporis? Id cupiditate, voluptatibus veniam doloribus consectetur quia! Quae, eveniet?
+                    </p>
 
-                <p>
-                    <h2>Education</h2>
-                    <h3>Front Range COmmunity College</h3>
-                    <ul>
-                        <li> Attended core curriculum at FRCC, earning a 3.2 GPA</li>
-                    </ul>
-                    <h3>University of Colorado Boulder</h3>
-                    <ul>
-                        <li>Attended CU Boulder earning a Bachelors in Software Engineering, aerning a 3.5 GPA</li>
-                        <li>Currently attending CU Boulder's Applied Sciences in Software Engineering Masters program</li>
-                    </ul>
+                    <ul><li>Google</li></ul>
+                    <p>Job Title: Software Developer<br/> 
+                    Date of Employement: 2/17/2013-8/15/2017<br/> 
+                    Job Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae veritatis sed laborum, accusantium ut dolore asperiores ea enim, hic fuga corporis? Id cupiditate, voluptatibus veniam doloribus consectetur quia! Quae, eveniet?</p>
+                </section>
 
-                    <h2>Work History</h2>
-                    <h3>Google</h3>
-                    <ul>
-                        <li>Dates: Oct 2017-Feb 2018</li>
-                        <li>Job Title: Intern</li>
-                        <li>Job desctription: lorem ipsum</li>
-                    </ul>
-                    <h3>Amazon</h3>
-                    <ul>
-                        <li>Dates: Feb 2018-present</li>
-                        <li>Job Title: Data Analyst</li>
-                        <li>Job Descrition: lorem ipsum</li>
-                    </ul>
-                </p>
-            </section>
-            <address>
-                <h2>Contact Info</h2>
-                <p>Twitter: <a href="https://twitter.com/sallyStudent">@sallyStudent</a></p>
-            </address>
-        </div>
+                <section>
+                    <h4>Contact Info</h4>
+                    <address>
+                        Name: Sally Student <br/>
+                        Email: <a href="mailto:SallyStudent@live.com?">SallyStudent@live.com</a><br/>
+                        Phone: <a href="tel:+1-888-555-1212">+1-888-555-1212</a>
+                    </address>
+                </section>
+            </main>        
+    </div>
     )
 }
 export default Info;
