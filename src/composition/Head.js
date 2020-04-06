500cf6e8d1fc9a177a79f10803ea7445c7d0ed9a
Cesar Morales <cesarlenin2@live.com>
2020-04-06 09:29:49 -0700

    added header

---
+++ src/composition/Head.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Head.css';
+
+function Head(){
+    return (
+        <header>
+            <h1>Sally Student resume</h1>
+            <img src="https://m.media-amazon.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_.jpg" alt="A picture of myself" />
+        </header>
+    )
+}
+
+export default Head;
