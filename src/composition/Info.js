6e2a40c6a9c8af938690ab355ec17c398a3c645b
Cesar Morales <cesarlenin2@live.com>
2020-04-06 08:55:58 -0700

    added the wireframe html

---
+++ src/composition/Info.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Info.css';
+
+function Info(props){
+    return(
+        <div>
+        <header>
+        <h1></h1>
+        <p></p>
+        </header>
+        <main></main>
+        <section></section>
+        <address>
+        <h2>Contact Info</h2>
+        <p>Twitter: <a href="https://twitter.com/sallyStudent">@sallyStudent</a></p>
+        </address>
+        </div>
+    )
+}
+export default Info;
